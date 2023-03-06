78ee57f1411887c23968ed0355fa27ca69ac07a7
vysalik98 <114649598+vysalik98@users.noreply.github.com>
2023-03-06 15:35:45 -0800

    Updated hw2 solutions and new Hw5 solutions

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -1,10 +1,45 @@
 const url =
-  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-let app = document.querySelector('#results');
+const app = document.querySelector("#results");
+const loading = document.querySelector("#loading");
 
-const fetchData = (url) => {
-  // Add your code here
+const addDrinkToDOM = (item) => {
+  let div = document.createElement("div");
+  div.className = "drinks";
+  app.appendChild(div);
+  let drink = document.createElement("div");
+  drink.className = "drink";
+
+  let image = document.createElement("img");
+  image.src = item.strDrinkThumb;
+  image.alt = `Non-alcoholic drink ${item.strDrink}`;
+  image.className = "figure";
+
+  let drinkname = document.createElement("h2");
+  drinkname.textContent = item.strDrink;
+  drinkname.className = "drinkname";
+
+  drink.append(image, drinkname);
+  app.querySelector(".drinks").appendChild(drink);
+};
+
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const drinks = data.drinks;
+    drinks.forEach((drink) => {
+      addDrinkToDOM(drink);
+    });
+  } catch (error) {
+    console.log(error);
+    const div = document.createElement("div");
+    div.textContent = `An error occurred. Please try again.`;
+    app.appendChild(div);
+  } finally {
+    app.removeChild(loading);
+  }
 };
 
 fetchData(url);
--- hw5/03-beverages.js
+++ hw5/03-beverages.js
@@ -1,6 +1,45 @@
 $(document).ready(function () {
   const url =
-    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-  // Add your code here
+  const app = $("#results");
+  const loading = $("#loading");
+
+  const addDrinkToDOM = (item) => {
+    let div = $("<div>").addClass("drinks");
+    app.append(div);
+    let drink = $("<div>").addClass("drink");
+    let image = $("<img>")
+      .attr({
+        src: item.strDrinkThumb,
+        alt: `Non-alcoholic drink ${item.strDrink}`,
+      })
+      .addClass("figure");
+
+    let drinkname = $("<h2>").text(item.strDrink).addClass("drinkname");
+    drink.append(image, drinkname);
+    app.find(".drinks").append(drink);
+  };
+
+  const fetchData = () => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: (data) => {
+        const drinks = data.drinks;
+        drinks.forEach((drink) => {
+          addDrinkToDOM(drink);
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        let div = $("<div>").text(`An error occurred. Please try again.`);
+        app.append(div);
+      },
+      complete: () => {
+        loading.remove();
+      },
+    });
+  };
+  fetchData(url);
 });
